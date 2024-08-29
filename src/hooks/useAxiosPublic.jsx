4132fe458f2725dfbb16178de148c48ce2adb1c3
roufrubel <rubel.ma2012@gmail.com>
2024-08-29 17:49:54 +0600

    initial set up

---
+++ src/hooks/useAxiosPublic.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const axiosPublic = axios.create({
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://assignment-12-medihealth-server.vercel.app",
+});
+
+const useAxiosPublic = () => {
+  return axiosPublic;
+};
+
+export default useAxiosPublic;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,19 @@
+import { Outlet, useLocation } from "react-router-dom";
+// import Footer from "../pages/shared/Footer/Footer";
+// import Navbar from "../pages/shared/Navbar/Navbar";
+
+
+const Main = () => {
+    const location = useLocation();
+    const noHeaderFooter = location.pathname.includes("login") || location.pathname.includes("signup");
+    return (
+        <div>
+            <h2>Main page</h2>
+            {/* {  noHeaderFooter || <Navbar></Navbar>}
+            <Outlet></Outlet>
+            {noHeaderFooter || <Footer></Footer>} */}
+        </div>
+    );
+};
+
+export default Main;
