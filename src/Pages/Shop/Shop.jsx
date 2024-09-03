590fd638dcb67d381bbcb2b878aa09e215081add
roufrubel <rubel.ma2012@gmail.com>
2024-09-03 19:02:40 +0600

    shop all category medicine added

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,10 +1,64 @@
+import { GrView } from "react-icons/gr";
+import useMedicine from "../../hooks/useMedicine";
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
 
 
 const Shop = () => {
+    const [medicine, loading] = useMedicine();
+    if(loading){
+        <progress className="progress w-56"></progress>
+    }
+
+
     return (
-        <div>
-            <h2>Shop page</h2>
+        <>
+        <div className="mt-0">
+        <Navbar></Navbar>
         </div>
+        <div className="pt-20">
+            <h2>All category medicine</h2>
+            <div className="overflow-x-auto">
+        <table className="table table-xs ">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>View</th>
+              <th>Select</th>
+            </tr>
+          </thead>
+          <tbody>
+            {medicine.map((data) => (
+                <tr key={data._id}>
+                  <td className="font-bold">{data?.name}</td>
+                  <td>{data?.category}</td>
+                  <td>${data?.price}</td>
+                {/* view */}
+                  <td>
+                  <Link to={`/medicine/${data._id}`}>
+                      <button className="btn btn-circle btn-sm btn-outline font-bold btn-warning">
+                        <GrView />
+                      </button>
+                    </Link>
+                  </td>
+                 
+                  <td>
+                      <button
+                        onClick={() => handleConfirm(data?._id)}
+                        className="btn btn-warning btn-xs"
+                      >
+                        Select
+                      </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+        </div>
+        </>
     );
 };
 
